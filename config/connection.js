d12f13ee575ef29ee2a7b7dc47ec8136b2a3d320
Hoo Man <hoomanfor@gmail.com>
2019-12-22 12:00:35 -0500

    added JAWSDB env variable to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
-// require("dotenv").config({ path: "../.env" });
 const mysql = require("mysql");
+let connection; 
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.PW,
-  database: "healthy_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.PW,
+    database: "healthy_db"
+  });
+}
 
 connection.connect(error => {
   if (error) {
